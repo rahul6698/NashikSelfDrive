5e21b54a5ca658fa0eade1659b384e78a7c0293b
rahul6698 <rahulkhode6@gmail.com>
2021-03-19 14:31:45 +0530

    initial commit

---
+++ application/views/common/header.php
@@ -0,0 +1,36 @@
+<header class="">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <a class="navbar-brand" href="index.html"><h2>Self Drive <em>Nashik</em></h2></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home
+                      <span class="sr-only">(current)</span>
+                    </a>
+                </li> 
+
+                <li class="nav-item"><a class="nav-link" href="services.php">Services</a></li>
+                <li class="nav-item"><a class="nav-link" href="offers.php">Offers</a></li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Select Car</a>
+                    
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="5seater.php">5 Seater</a>
+                      <a class="dropdown-item" href="7seater.php">7 Seater</a>
+                    </div>
+                </li>
+
+                <li class="nav-item"><a class="nav-link" href="about-us.php">About Us</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact.php">Contact Us</a></li>
+				<li class="nav-item"><a class="" href="services.php"><button type="button" class="btn btn-secondary">Book Now</button>
+		</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
---
+++ application/views/index.php
@@ -0,0 +1,468 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="<?php echo base_url()?>assets/images/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>Self Drive Nashik</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url()?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/fontawesome.css">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/owl.css">
+	<style>
+	.card {
+		margin:10px;
+		border-radius:10px;
+	}
+	.fa-car:hover{
+		color:red;
+	}
+	</style>
+  </head>
+
+  <body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+
+    <!-- Header -->
+    <?php include_once 'common/header.php' ?>
+    <!-- Page Content -->
+    <!-- Banner Starts Here -->
+    <div class="banner header-text">
+      <div class="owl-banner owl-carousel">
+        <div class="banner-item-01">
+          <div class="text-content">
+			 <div class="bbbootstrap">
+				 <div class="container">
+					 <form>
+						 <span role="status" aria-live="polite" class="ui-helper-hidden-accessible"></span><input style="opacity:0.3;" type="text" id="Form_Search" value="" placeholder="Search for your favorite car" role="searchbox" class="InputBox " autocomplete="off"><input type="submit" id="Form_Go" class="btn" value="Search">
+					 </form>
+				 </div>
+			 </div>
+		  <!--<h2>Self Drive Car Rental in Nashik</h2>-->
+          </div>
+        </div>
+        <div class="banner-item-02">
+          <div class="text-content">
+            <h2>Select your car today!& Enjoy Your Journy</h3>
+          </div>
+        </div>
+        <div class="banner-item-03">
+          <div class="text-content">
+            <h4>DRIVE IN A SANITISED CAR</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Banner Ends Here -->
+	<div class="container">
+		<center><br>
+			<h2 style="color: #666;">How Self-Drive Works</h2>
+			<p>Drive yourself with 4 simple steps!!!</p>
+		<div class="card-deck">
+		  <div class="card ">
+			<div class="card-body text-center">
+			<img class="lazy-img" alt="Book" data-src="car-icon.svg" src="assets/images/car-icon.svg">
+			  <h4>Book Car</h4>
+			  <p class="card-text">Search car and book on our site or App!</p>
+			</div>
+		  </div>
+		  <div class="card ">
+			<div class="card-body text-center">
+			  <img class="lazy-img" alt="license" data-src="license.svg" src="assets/images/license.svg">
+			  <h4>Driving License</h4>
+			  <p class="card-text">Upload your driver’s license, and pay a small security deposit.</p>
+			</div>
+		  </div>
+		  <div class="card ">
+			<div class="card-body text-center">
+			  <img class="lazy-img" alt="car" data-src="car.svg" src="assets/images/car.svg">
+			  <h4>Start Journy</h4>
+			  <p class="card-text">Fill the start checklist in the app. Grab the keys and drive.</p>
+			</div>
+		  </div>
+		  <div class="card">
+			<div class="card-body text-center">
+			  <img class="lazy-img" alt="return" data-src="return.svg" src="assets/images/return.svg">
+			  <h4>Return car</h4>
+			  <p class="card-text">Return the car to the same location and fill the end checklist to end your trip.</p>
+			</div>
+		  </div>
+		</div><br>
+		<button type="button" class="btn btn-secondary btn-lg">Book Now</button>
+		</center>
+	</div>
+	<!-- how it works end  -->
+	<div class="latest-products">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <h2>Offers</h2>
+              <a href="offers.html">view more <i class="fa fa-angle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="5seater.php"><img src="<?php echo base_url()?>assets/images/swift.png" alt=""></a>
+              <div class="down-content">
+                <a href="5seater.php"><h4>Maruti Swift</h4></a>
+                <h6><small>from</small> ₹1,999/-<small>per Day</small></h6>
+                <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="mpv.php"><img src="<?php echo base_url()?>assets/images/innova.jpg" alt=""></a>
+              <div class="down-content">
+                <a href="mpv.php"><h4>Toyota Innova Crista </h4></a>
+                <h6><small>from</small> ₹2,999/- <small>per Day</small></h6>
+                <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="product-item">
+              <a href="xuv.php"><img src="<?php echo base_url()?>assets/images/creta.jpg" height="218px" alt=""></a>
+              <div class="down-content">
+                <a href="xuv.php"><h4>Hyundai Creta</h4></a>
+                <h6><small>from</small> ₹2,499/-  <small>per Day</small></h6>
+                <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--<div class="best-features">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <h2>About Us</h2>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="left-content">
+              <p>Lorem ipsum dolor sit amet, <a href="#">consectetur</a> adipisicing elit. Explicabo, esse consequatur alias repellat in excepturi inventore ad <a href="#">asperiores</a> tempora ipsa. Accusantium tenetur voluptate labore aperiam molestiae rerum excepturi minus in pariatur praesentium, corporis, aliquid dicta.</p>
+              <ul class="featured-list">
+                <li><a href="#">Lorem ipsum dolor sit amet</a></li>
+                <li><a href="#">Consectetur an adipisicing elit</a></li>
+                <li><a href="#">It aquecorporis nulla aspernatur</a></li>
+                <li><a href="#">Corporis, omnis doloremque</a></li>
+              </ul>
+              <a href="about-us.html" class="filled-button">Read More</a>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="right-image">
+              <img src="assets/images/about-1-570x350.jpg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> -->
+
+    <div class="services" style="background-image: url(<?php echo base_url()?>assets/images/other-image-fullscren-1-1920x900.jpg);" >
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <h2>Latest blog posts</h2>
+
+              <a href="blog.html">read more <i class="fa fa-angle-right"></i></a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6">
+            <div class="service-item">
+              <a href="#" class="services-item-image"><img width="100%" height="200px" src="assets/images/cretauser.jpg" class="img-fluid" alt=""></a>
+              <div class="down-content">                
+				<h4>Mr.Shrikant Kshatriya</h4><a><span style="text-align: justify;">Driving experience is too awesome Even I can drive thousands of kilometres in this Car. Most reliable car innova</p></a></h4>
+
+                <p style="margin: 0;"><i class="fa fa-map-marker" aria-hidden="true"></i> Pune &nbsp;&nbsp;|&nbsp;&nbsp; 12/06/2020 10:30 &nbsp;&nbsp;|&nbsp;&nbsp; 3 days</p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-lg-4 col-md-6">
+            <div class="service-item">
+              <a href="#" class="services-item-image"><img width="100%" height="200px" src="assets/images/swiftuser.jpg" class="img-fluid" alt=""></a>
+              <div class="down-content">                
+				<h4>Mr.Rahul Khode</h4><a><span style="text-align: justify;">Driving experience is too awesome Even I can drive thousands of kilometres in this Car. Most reliable car innova</p></a></h4>
+
+                <p style="margin: 0;"><i class="fa fa-map-marker" aria-hidden="true"></i> Mumbai &nbsp;&nbsp;|&nbsp;&nbsp; 12/06/2020 10:30 &nbsp;&nbsp;|&nbsp;&nbsp; 1 days</p>
+              </div>
+            </div>
+          </div>
+		  <div class="col-lg-4 col-md-6">
+            <div class="service-item">
+              <a href="#" class="services-item-image"><img width="100%" height="200px" src="assets/images/cretauser.jpg" class="img-fluid" alt=""></a>
+              <div class="down-content">                
+				<h4>Mr.Shubham Joshi</h4><a><span style="text-align: justify;">Driving experience is too awesome Even I can drive thousands of kilometres in this Car. Most reliable car innova</p></a></h4>
+
+                <p style="margin: 0;"><i class="fa fa-map-marker" aria-hidden="true"></i> Lonavala &nbsp;&nbsp;|&nbsp;&nbsp; 12/06/2020 10:30 &nbsp;&nbsp;|&nbsp;&nbsp; 6 days</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+	<!-- Advantages -->
+	<div class="container">
+	  <center><br>
+			<h2 style="color: #d3d3d3;">Advantages of Self-Drive </h2>
+			<p style="color: #d0d0d0;">We simplified car rentals, so you can focus on what's important to you.</p>
+			
+		<div class="card-deck container">
+		  <div class="card" style="border-radius:0px;">
+			<div class="card-body text-center">
+			<img class="lazy-img" alt="Book" data-src="fuel.png" width="50px" height="50px" src="assets/images/fuel.png">
+			  <h4>Fuel Cost Included</h4>
+			  <p class="card-text">Search car and book on our site or App!</p>
+			</div>
+		  </div>
+		  <div class="card ">
+			<div class="card-body text-center">
+			  <img class="lazy-img" alt="license" data-src="license.svg" width="50px" height="50px" src="assets/images/hidden.png">
+			  <h4>No Hidden Charges</h4>
+			  <p class="card-text">Our prices include taxes and insurance.What you see is what you really pay!</p>
+			</div>
+		  </div>
+		  <div class="card">
+			<div class="card-body text-center">
+			  <img class="lazy-img" alt="return" data-src="return.svg" width="50px" height="50px" src="assets/images/flexi.png">
+			  <h4>Flexi Pricing Packages</h4>
+			  <p class="card-text">One size never fits all! Choose a balance of time and kilometers that works best for you.</p>
+			</div>
+		  </div>
+		  </div><br>
+		  <div class="card-deck container">
+		  <div class="card ">
+			<div class="card-body text-center">
+			<img class="lazy-img" alt="Book" data-src="car-icon.svg" width="50px" height="50px" src="assets/images/location.jpg">
+			  <h4>Destiny</h4>
+			  <p class="card-text">Our cars have all-India permits.Just remember to pay state tolls and entry taxes.</p>
+			</div>
+		  </div>
+		  <div class="card ">
+			<div class="card-body text-center">
+			  <img class="lazy-img" alt="license" data-src="license.svg" width="50px" height="50px" src="assets/images/road.jpg">
+			  <h4>24*7 Service</h4>
+			  <p class="card-text">We have round-the-clock, pan India partners. Help is never far away from you.</p>
+			</div>
+		  </div>
+		  <div class="card">
+			<div class="card-body text-center">
+			  <img class="lazy-img" alt="return" data-src="return.svg" width="50px" height="50px" src="assets/images/damage.png">
+			  <h4>Insurance</h4>
+			  <p class="card-text">All your bookings include damage insurance! Drive safe, but don’t worry!</p>
+			</div>
+		  </div>
+		  </div><br>
+		  
+		<button type="button" class="btn btn-secondary btn-lg">Book Your car</button>
+		</center>
+	</div>
+	
+    <div class="happy-clients">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <h2>Happy Clients</h2>
+
+              <a href="testimonials.html">read more <i class="fa fa-angle-right"></i></a>
+            </div>
+          </div>
+          <div class="col-md-12">
+            <div class="owl-clients owl-carousel text-center">
+              <div class="service-item">
+                <div class="icon"style="padding:3px;">
+				<center>
+                  <img src="<?php echo base_url()?>assets/images/shree.jpg" alt="Avatar" style="width:180px;height:180px;border-radius: 50%;">
+                </center>
+                </div>
+                <div class="down-content">
+                  <h4>Shrikant Kshatriya</h4>
+                  <p class="n-m"><em>"Lorem ipsum dolor sit amet, consectetur an adipisicing elit. Itaque, corporis nulla at quia quaerat."</em></p>
+                </div>
+              </div>
+              
+              <div class="service-item">
+                <div class="icon"style="padding:3px;">
+				<center>
+                  <img src="<?php echo base_url()?>assets/images/user.png" alt="Avatar" style="width:180px;border-radius: 50%;">
+                </center>
+                </div>
+                <div class="down-content">
+                  <h4>Shubham Joshi</h4>
+                  <p class="n-m"><em>"Lorem ipsum dolor sit amet, consectetur an adipisicing elit. Itaque, corporis nulla at quia quaerat."</em></p>
+                </div>
+              </div>
+              
+              <div class="service-item">
+                <div class="icon" style="padding:3px;">
+				<center>
+                  <img src="<?php echo base_url()?>assets/images/user.png" alt="Avatar" style="width:180px;border-radius: 50%;">
+                </center>
+				</div>
+                <div class="down-content">
+                  <h4>Rahul Khode</h4>
+                  <p class="n-m"><em>"Lorem ipsum dolor sit amet, consectetur an adipisicing elit. Itaque, corporis nulla at quia quaerat."</em></p>
+                </div>
+              </div>
+              
+              <div class="service-item">
+                <div class="icon">
+                  <i class="fa fa-user"></i>
+                </div>
+                <div class="down-content">
+                  <h4>John Doe</h4>
+                  <p class="n-m"><em>"Lorem ipsum dolor sit amet, consectetur an adipisicing elit. Itaque, corporis nulla at quia quaerat."</em></p>
+                </div>
+              </div>
+              
+              <div class="service-item">
+                <div class="icon">
+                  <i class="fa fa-user"></i>
+                </div>
+                <div class="down-content">
+                  <h4>Jane Smith</h4>
+                  <p class="n-m"><em>"Lorem ipsum dolor sit amet, consectetur an adipisicing elit. Itaque, corporis nulla at quia quaerat."</em></p>
+                </div>
+              </div>
+              
+              <div class="service-item">
+                <div class="icon">
+                  <i class="fa fa-user"></i>
+                </div>
+                <div class="down-content">
+                  <h4>Antony Davis</h4>
+                  <p class="n-m"><em>"Lorem ipsum dolor sit amet, consectetur an adipisicing elit. Itaque, corporis nulla at quia quaerat."</em></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="call-to-action">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="inner-content">
+              <div class="row">
+                <div class="col-md-8">
+                  <h4>Mr.Shubham Joshi,</h4>
+                  <p>Select your car today & Enjoy Your Journy with Self-Drive-car in Sanitised Car.</p>
+                </div>
+                <div class="col-lg-4 col-md-6 text-right">
+                  <a href="contact.html" class="filled-button">Contact Us</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+<footer style="background-color:black;"class=" text-white text-center text-lg-start">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!--Grid row-->
+    <div class="row">
+      <!--Grid column-->
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h5 class="text-uppercase">Footer Content</h5>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+          molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
+          voluptatem veniam, est atque cumque eum delectus sint!
+        </p>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        
+
+        <ul class="list-unstyled mb-0">
+          <li>
+            <a href="#!" class="text-white">About Us</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Contact Us</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Car Types</a>
+          </li>
+          <li>
+            <a href="#!" class="text-white">Loaction</a>
+          </li>
+        </ul>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase mb-0">Links</h5>
+			<div class="f_social_icon">
+				<a href="#" class="fab fa-facebook"></a>
+				<a href="#" class="fab fa-twitter"></a>
+				<a href="#" class="fab fa-linkedin"></a>
+				<a href="#" class="fab fa-pinterest"></a>
+			</div>
+		</div>
+	</div>
+      </div>
+      <!--Grid column-->
+    </div>
+    <!--Grid row-->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+    © 2021 Copyright:
+    <a class="text-white" href="https://selfdrivecar.com/">selfdrivecar.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?php echo base_url()?>assets/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url()?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+    <!-- Additional Scripts -->
+    <script src="<?php echo base_url()?>assets/js/custom.js"></script>
+    <script src="<?php echo base_url()?>assets/js/owl.js"></script>
+  </body>
+</html>
